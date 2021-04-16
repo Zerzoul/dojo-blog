dd01e02b2f100e70f90e179631111c6a3397607b
ZerZoul <a.adnot@gmail.com>
2021-04-16 07:58:38 +0200

    reastructuration and delete unusefull file

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        App Component
+      </div>
+    </div>
+  );
+}
+
+export default App;
