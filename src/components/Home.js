7dc750dfea6df50f78e74a507ccf3c160fecbc3f
ZerZoul <a.adnot@gmail.com>
2021-04-16 14:16:46 +0200

    use useState for event click

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
+import {useState} from 'react'
+
 const Home = () => {
+    const [click, setClick] = useState(0)
+
+    const handleClick = () => {
+        let manyClick = click
+        setClick(manyClick += 1 ) 
+    }
+
     return (
         <div className="home">
-            <h1>Home Page</h1>
+            <h2>Home Page</h2>
+            <p>{` you click ${click} times`}</p>
+            <button onClick={handleClick}>Click on me</button>
         </div>
     );
 }
