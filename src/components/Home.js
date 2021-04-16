ab61263db86f4d35bf342ac5549112e4fdb07c13
ZerZoul <a.adnot@gmail.com>
2021-04-16 14:33:50 +0200

    add the blogs state

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,11 @@ import {useState} from 'react'
 
 const Home = () => {
     const [click, setClick] = useState(0)
-
+    const  [blogs, setBlogs] = useState([
+        {title: "My new website", body:"lorem ipsum...", author:"mario", id: 1 },
+        {title: "Welcome party!", body:"lorem ipsum...", author:"yoshi", id: 2 },
+        {title: "Web dev top tips", body:"lorem ipsum...", author:"mario", id: 3 },
+    ])
     const handleClick = () => {
         let manyClick = click
         setClick(manyClick += 1 ) 
@@ -10,9 +14,21 @@ const Home = () => {
 
     return (
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{` you click ${click} times`}</p>
-            <button onClick={handleClick}>Click on me</button>
+            <div>
+                <h2>Home Page</h2>
+                <p>{` you click ${click} times`}</p>
+                <button onClick={handleClick}>Click on me</button>
+            </div>
+            <div>
+                {
+                    blogs.map((blog) => (
+                        <div className="blog-preview" key={blog.id}>
+                            <h2>{blog.title}</h2>
+                            <p>Written by {blog.author}</p>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 }
