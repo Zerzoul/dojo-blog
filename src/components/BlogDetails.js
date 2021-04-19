135f4006a6a367ecf80c2f844a8a169c57956df7
ZerZoul <a.adnot@gmail.com>
2021-04-19 12:04:12 +0200

    create 404Pages, create a deleted button

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams()
+    const url = "http://localhost:8000/blogs/"
+    const {data,errorMessage,isPending} = useFetch(url + id);
+    const history = useHistory();
 
-    const {data,errorMessage,isPending} = useFetch("http://localhost:8000/blogs/" + id);
+    const handleClick = () => {
+        fetch(url + id,{
+            method:"DELETE",
+        }).then(()=>{
+            history.push('/')
+        })
+    }
     return (
         <div className="blog-details">
             { errorMessage && <div>{errorMessage}</div> }
@@ -14,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{data.title}</h2>
                     <p>Written by {data.author}</p>
                     <div>{data.body}</div>
+                    <button onClick={handleClick} >Delete</button>
                 </article>
             )}
         </div>
